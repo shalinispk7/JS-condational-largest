11c9031db4cc4712ed8b29c405bc10e486f0e9a4
shalinispk7 <shalinispk7@gmail.com>
2023-05-29 19:20:05 +0530

    comparing two numbers

---
+++ main.js
@@ -0,0 +1,12 @@
+let a= parseInt(prompt("Enter value a"));
+let b= parseInt(prompt("Enter value b"));
+let c= parseInt(prompt("Enter value c"));
+if (a>b && a>c){
+document.write("Value A is greater");
+}
+else if (b>a && b>c){
+document.write("Value B is greater");
+}
+else if (c>a && c>b){
+document.write("Value C is greater");
+}
